1c3c2d11ee835181732f2e47cf1c940b1dc6b675
Olivia Finster <olivia.finster@gmail.com>
2021-06-05 14:15:56 -0500

    conditional render of image swap to description successfully toggles.

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,30 +1,45 @@
 import axios from 'axios';
 import './GalleryItem.css';
+import { useState } from 'react';
 
-function GalleryItem (props) {
+function GalleryItem(props) {
     // console.log('logging props.picture.path in GalleryItem', props.picture.path);
-    console.log('in galleryitem, logging getGallery', props.getGallery);
+    // console.log('in galleryitem, logging getGallery', props.getGallery);
+
+    const [isHidden, setIsHidden] = useState(false);
+
     const handleLike = () => {
         // console.log(props.picture);
-        // const data = {
-
-        // }
         axios.put(`/gallery/like/${props.picture.id}`)
-        .then( response => {
-            console.log('response in put', response);
-            props.getGallery();
-        })
-        .catch(error => {
-            console.log('error in put', error);
-        })
+            .then(response => {
+                console.log('response in put', response);
+                props.getGallery();
+            })
+            .catch(error => {
+                console.log('error in put', error);
+            })
     }
 
-    return(
-        <div className="itemBox">
-            <img className="galleryImage" src={props.picture.path} />
-            <button onClick={handleLike} className="btn btn-dark btn-sm">Like</button>
-            <p>Number of likes: {props.picture.likes}</p>
-        </div>
+    const handleImageClick = () => {
+        setIsHidden(!isHidden);
+    }
+    console.log('logging is hidden', isHidden);
+    return (
+        <>
+            <div className="itemBox">
+                {isHidden ? (<div>
+                    <p onClick={handleImageClick}>{props.picture.description}</p>
+
+                </div>
+                ) : (
+                    <div>
+                        <img onClick={handleImageClick} className="galleryImage" src={props.picture.path} />
+                    </div>
+                )}
+                <button onClick={handleLike} className="btn btn-dark btn-sm">Like</button>
+                <p>Number of likes: {props.picture.likes}</p>
+            </div>
+        </>
     )
 }
 
