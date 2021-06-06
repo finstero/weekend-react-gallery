a4689a08720bc8a6c16ed7efde6be04da9373239
Olivia Finster <olivia.finster@gmail.com>
2021-06-05 20:10:14 -0500

    small tweaks.

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -20,18 +20,11 @@ router.put('/like/:id', (req, res) => {
       res.sendStatus(500);
     })
 
-
-    // for(const galleryItem of galleryItems) {
-    //     if(galleryItem.id == galleryId) {
-    //         galleryItem.likes += 1;
-    //     }
-    // }
-    // res.sendStatus(200);
 }); // END PUT Route
 
 // GET Route
 router.get('/', (req, res) => {
-    let queryText = 'SELECT * FROM "gallery";';
+    let queryText = 'SELECT * FROM "gallery" ORDER BY "description";';
     pool.query(queryText).then(result => {
         res.send(result.rows);
     })
